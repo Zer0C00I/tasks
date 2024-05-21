132c51a28c09fbf1c7ff51128b8ade6dec554a0c
Zer0C00I <your_email@example.com>
2024-05-22 02:40:17 +0300

    task7

--- task7/main.cpp
+++ task7/main.cpp
@@ -5,36 +5,38 @@
 #include <QLabel>
 #include <QString>
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
     QApplication app(argc, argv);
 
     QWidget window;
     window.setWindowTitle("Calculator");
 
-    // Display
     QLabel *display = new QLabel("0");
     display->setAlignment(Qt::AlignRight);
     display->setFixedHeight(50);
 
-    
     QGridLayout *gridLayout = new QGridLayout;
     QString buttonLabels[16] = {"7", "8", "9", "/",
                                 "4", "5", "6", "*",
                                 "1", "2", "3", "-",
                                 "C", "0", "=", "+"};
 
-    for (int i = 0; i < 16; ++i) {
+    for (int i = 0; i < 16; ++i)
+    {
         QPushButton *button = new QPushButton(buttonLabels[i]);
         button->setFixedSize(50, 50);
         gridLayout->addWidget(button, i / 4, i % 4);
 
-        // Connect buttons to slots
-        if (buttonLabels[i] == "C") {
-            QObject::connect(button, &QPushButton::clicked, [=]() {
-                display->setText("0");
-            });
-        } else if (buttonLabels[i] == "=") {
-            QObject::connect(button, &QPushButton::clicked, [=]() {
+        if (buttonLabels[i] == "C")
+        {
+            QObject::connect(button, &QPushButton::clicked, [=]()
+                             { display->setText("0"); });
+        }
+        else if (buttonLabels[i] == "=")
+        {
+            QObject::connect(button, &QPushButton::clicked, [=]()
+                             {
                 QString expression = display->text();
                 QRegExp regex("(\\+|\\-|\\*|\\/)");
                 QStringList operands = expression.split(regex);
@@ -49,20 +51,20 @@ int main(int argc, char *argv[]) {
                     else if (expression.contains("/"))
                         result = operands[0].toDouble() / operands[1].toDouble();
                     display->setText(QString::number(result));
-                }
-            });
-        } else {
-            QObject::connect(button, &QPushButton::clicked, [=]() {
+                } });
+        }
+        else
+        {
+            QObject::connect(button, &QPushButton::clicked, [=]()
+                             {
                 if (display->text() == "0" || display->text() == "Error") {
                     display->setText(button->text());
                 } else {
                     display->setText(display->text() + button->text());
-                }
-            });
+                } });
         }
     }
 
-    
     QVBoxLayout *layout = new QVBoxLayout;
     layout->addWidget(display);
     layout->addLayout(gridLayout);
@@ -71,4 +73,3 @@ int main(int argc, char *argv[]) {
     window.show();
     return app.exec();
 }
-
